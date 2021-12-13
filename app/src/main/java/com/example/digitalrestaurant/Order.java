c0c9e11638c79db1a874b8c08edec6f37308e7e3
Onah <thisaremyfiles@gmail.com>
2021-12-13 21:50:24 +0000

    Adjusted and changed some drawable files

--- app/src/main/java/com/example/digitalrestaurant/Order.java
+++ app/src/main/java/com/example/digitalrestaurant/Order.java
@@ -86,7 +86,10 @@ public class Order extends AppCompatActivity {
 
 
         orderPrice=findViewById(R.id.orderPrice);
+        orderNationality=findViewById(R.id.orderNationality);
+        orderFoodName=findViewById(R.id.orderFoodName);
         adaFoodImages=findViewById(R.id.adarestaurantcard);
+        restaurantNameA=findViewById(R.id.orderRestaurant);
         quantityText=findViewById(R.id.quantityTxt);
         addToCartBtn=findViewById(R.id.addToCartbtn);
         plusBtn=findViewById(R.id.plusBtn);
